b9fe6182b5f4f8847a4bb0dd26f88c56c698c318
noodulf <jaintarun926@gmail.com>
2023-11-10 17:53:29 +0530

    understood auth

--- noodlemuse/server.js
+++ noodlemuse/server.js
@@ -1,9 +1,28 @@
 const http = require('http');
+const fetch= require('node-fetch');
 
+const encAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
+const CLIENT_SECRET='70db1e28e4444e1198f2b9fd9484e8e1';
+
+function auth(res){
+    const CLIENT_ID='1969d396ab164fb7834ca366b2ae4ec8';
+    const redirectUri = 'http://localhost:3000/';
+    const scopes = 'user-read-private user-read-email playlist-read-private';
+    const authUrl=`https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`
+    res.writeHead(302,{
+        'Location':authUrl
+    })
+}
 
 const server = http.createServer((req, res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Working\n');
+    const {url, method}= req;
+    let content= "This text was processed successfully"
+    switch (url){
+        case '/': console.log("Home page");
+        break;
+        case '/authorize': auth(res)
+    }
+
 })
 
 server.listen(3000, ()=>{
