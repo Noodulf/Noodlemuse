5b5aded7bcffdb36f47779a36a9cacfb6f999869
noodulf <jaintarun926@gmail.com>
2023-11-10 19:09:41 +0530

    working server

--- noodlemuse/server.js
+++ noodlemuse/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const fetch= require('node-fetch');
+// const fetch= require('node-fetch');
 
-const encAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
+// const encAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
 const CLIENT_SECRET='70db1e28e4444e1198f2b9fd9484e8e1';
 
 function auth(res){
@@ -11,16 +11,22 @@ function auth(res){
     const authUrl=`https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`
     res.writeHead(302,{
         'Location':authUrl
-    })
+    });
+    res.end("Authorization worked");
 }
 
 const server = http.createServer((req, res)=>{
     const {url, method}= req;
     let content= "This text was processed successfully"
     switch (url){
-        case '/': console.log("Home page");
+        case '/': res.writeHead(200,{'Content-type':'text/plain'})
+        console.log("Home page");
+        res.end('Homepage')
         break;
         case '/authorize': auth(res)
+        break;
+        default: res.writeHead(404,{'Content-type':'text/plain'});
+                 res.end("404")
     }
 
 })
