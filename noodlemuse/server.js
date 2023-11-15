6557dd3d18449a3463af41d63ea89056c54a65fb
noodulf <jaintarun926@gmail.com>
2023-11-15 22:41:10 +0530

    Hardcoded server. cant get token

--- noodlemuse/server.js
+++ noodlemuse/server.js
@@ -1,44 +1,113 @@
 const http = require('http');
 const url = require('url');
-// const fetch= require('node-fetch');
+// const fetch = require('node-fetch');
+const querystring = require('querystring');
 
-// const encAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
-const CLIENT_SECRET='70db1e28e4444e1198f2b9fd9484e8e1';
 
-function auth(res){
-    const CLIENT_ID='1969d396ab164fb7834ca366b2ae4ec8';
+function secret() {
+    const CLIENT_ID = '1969d396ab164fb7834ca366b2ae4ec8';
+    const CLIENT_SECRET = '70db1e28e4444e1198f2b9fd9484e8e1';
     const redirectUri = 'http://localhost:3000/callback';
+
+    function getSecrets() {
+        return {
+            CLIENT_ID,
+            CLIENT_SECRET,
+            redirectUri
+        }
+    }
+    return getSecrets;
+}
+const func1 = secret();
+const secrets = func1();
+
+
+function auth(res) {
     const scopes = 'user-read-private user-read-email playlist-read-private';
-    const authUrl=`https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`
-    res.writeHead(302,{
-        'Location':authUrl
+    const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${secrets.CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(secrets.redirectUri)}`
+    res.writeHead(302, {
+        'Location': authUrl
     });
     res.end("Authorization worked");
 }
 
-const server = http.createServer((req, res)=>{
+
+function accToken(code) {
+
+    const options = {
+        hostname: 'accounts.spotify.com',
+        path: '/api/token',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Basic ' + (Buffer.from(secrets.CLIENT_ID + ':' + secrets.CLIENT_SECRET).toString('base64'))
+        }
+
+    }
+
+    const postData = querystring.stringify({
+        code: code,
+        redirect_uri: secrets.redirectUri,
+        grant_type: 'authorization_code'
+    })
+    const req = http.request(options,(response)=>{
+        let data='';
+        
+        response.on('data',(chunk)=>{
+            data+=chunk;
+            console.log('Received data chunk:', chunk);
+        })
+
+        // response.on('end',()=>{
+        //     const tokenData= JSON.parse(data);
+        //     console.log('Token data is:',tokenData)
+        // })
+
+        response.on('end', () => {
+            try {
+                // const tokenData = JSON.parse(data);
+                console.log('Token data is:', data);
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+                // Handle JSON parsing error here
+            }
+        });        
+        
+    })
+
+    req.write(postData);
+    req.end();
+
+}
+
+
+
+const server = http.createServer((req, res) => {
     const { pathname, query } = url.parse(req.url, true);
-    let content= "This text was processed successfully"
-    switch (pathname){
-        case '/': res.writeHead(200,{'Content-type':'text/plain'})
-        console.log("Home page");
-        res.end('Homepage')
-        break;
+    let content = "This text was processed successfully"
+    switch (pathname) {
+        case '/': res.writeHead(200, { 'Content-type': 'text/plain' })
+            console.log("Home page");
+            res.end('Homepage')
+            break;
         case '/authorize': auth(res)
-        break;
+            break;
         case '/callback': console.log("callback initiated");
             console.log("Query parameters:", query)
-            res.writeHead(302,{
-            'Location':'/'
-        })
-        res.end()
-        break;
-        default: res.writeHead(404,{'Content-type':'text/plain'});
-                 res.end("404")
+            const code = query.code;
+            accToken(code);
+            res.writeHead(302, {
+                'Location': '/'
+            })
+            res.end()
+            break;
+        default: res.writeHead(404, { 'Content-type': 'text/plain' });
+            console.log("404")
+            res.end("404")
     }
 
 })
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log("Listening");
 })
