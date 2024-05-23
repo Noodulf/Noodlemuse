0b1cd447a0496b7f14f9d71fab165476c354bf09
noodulf <jaintarun926@gmail.com>
2024-05-23 15:27:15 +0530

    Able to retrieve weekly top artists

--- noodlemuse/server.js
+++ noodlemuse/server.js
@@ -3,7 +3,6 @@ import url from 'url';
 import express from 'express';
 import fetch from 'node-fetch';
 
-const app= express();
 
 function secret() {
     const CLIENT_ID = '1969d396ab164fb7834ca366b2ae4ec8';
@@ -24,7 +23,7 @@ const secrets = func1();
 
 
 function auth(res) {
-    const scopes = 'user-read-private user-read-email playlist-read-private';
+    const scopes = 'user-read-private user-read-email playlist-read-private user-top-read';
     const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${secrets.CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(secrets.redirectUri)}`
     res.writeHead(302, {
         'Location': authUrl
@@ -49,7 +48,6 @@ async function getTokenData(code) {
             'Authorization': 'Basic ' + (Buffer.from(secrets.CLIENT_ID + ':' + secrets.CLIENT_SECRET).toString('base64'))
         },
         body: postData
-
     }
 
     try {
@@ -67,39 +65,69 @@ async function getTokenData(code) {
 
 }
 
-function accToken(code, res) {
-    getTokenData(code)
-        .then((tokenData) => {
-            console.log('Token data:', tokenData);
+async function accToken(code, res) {
+    try {
+        const tokenData = await getTokenData(code)
 
-            res.writeHead(302, {
-                'Location': '/'
-            })
-            res.end()
+        console.log('Token data:', tokenData);
+        res.writeHead(302, {
+            'Location': '/'
         })
-        .catch((error) => {
-            console.error('Error getting token data:', error);
-            // Handle errors here
-        });
+        res.end()
+        return tokenData;
+    }
+    catch (error) {
+        console.error('Error getting token data:', error);
+    };
 }
 
+async function topArtists(token) {
+    console.log("tA function called",token.access_token);
 
-const server = http.createServer((req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token.access_token}`
+        }
+    }
+    try {
+        const response = await fetch('https://api.spotify.com/v1/me/top/artists?time_range=short_term', options);
+        console.log("response: ", response);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data from Spotify API: ${response.statusText}`);
+        }
+        const data = await response.json();
+        console.log("Names in order:");
+        for (let i = 0; i < data.items.length; i++) {
+            console.log(`Artist at ${i + 1}: ${data.items[i].name}`);
+        }
+    }
+    catch (error) {
+        console.error('Error getting token data:', error);
+    }
+}
+
+const app = express();
+const server = http.createServer(async (req, res) => {
     const { pathname, query } = url.parse(req.url, true);
+    var token;
     switch (pathname) {
         case '/': res.writeHead(200, { 'Content-type': 'text/plain' })
             console.log("Home page");
             res.end('Homepage')
             break;
         case '/authorize': auth(res)
+            console.log("authorize initiated");
             break;
         case '/callback': console.log("callback initiated");
             console.log("Query parameters:", query)
             const code = query.code;
-            accToken(code, res);
+            token = await accToken(code, res);
+            console.log("token in routes:", token);
+            await topArtists(token); 
             break;
         default: res.writeHead(404, { 'Content-type': 'text/plain' });
-            console.log("404")
+            console.log("404 forced", pathname)
             res.end("404")
     }
 
